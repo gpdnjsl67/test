53f5bb9f0cc9bfb72e78c8a3ae0a48ea5ebd4045
gpdnjsl67 <101319275+gpdnjsl67@users.noreply.github.com>
2024-10-23 17:27:02 +0900

    Add files via upload

--- Bootstrap_new/js/modal.js
+++ Bootstrap_new/js/modal.js
@@ -1,13 +1,19 @@
+const fadeDuration = 300; // or fadeTime, animationDuration
+
 // 모달창 열기
 function modalOpen(modalBox, overlay) {
-    modalBox.fadeIn(300);
-    overlay.fadeIn(300);
+    modalBox.fadeIn(fadeDuration);
+    overlay.fadeIn(fadeDuration);
+    modalBox.find('.bs-modal__content').scrollTop(0);
     $('body').css('overflow', 'hidden');
 }
 
 // 모달창 닫기
 function modalClose(modalBox, overlay) {
-    modalBox.fadeOut(300);
-    overlay.fadeOut(300);
+    modalBox.fadeOut(fadeDuration);
+    overlay.fadeOut(fadeDuration);
+    modalBox.fadeOut(fadeDuration, function () {
+        modalBox.find('.bs-modal__content').scrollTop(0);
+    });
     $('body').css('overflow', 'auto');
 }
